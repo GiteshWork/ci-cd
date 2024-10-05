a37ab2710e02bd6ef0d759ca008f0ef18a3d21d0
gitesh <work.giteshpradhan@gmail.com>
2024-10-05 16:13:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Sample CRUD routes
+app.get('/', (req, res) => res.send('Hello, CI/CD World!'));
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+// CRUD Routes for "items"
+let items = [];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+  const item = req.body;
+  items.push(item);
+  res.status(201).json(item);
+});
+
+app.put('/items/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const itemIndex = items.findIndex(i => i.id === id);
+
+  if (itemIndex !== -1) {
+    items[itemIndex] = req.body;
+    res.json(items[itemIndex]);
+  } else {
+    res.status(404).send('Item not found');
+  }
+});
+
+app.delete('/items/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  items = items.filter(i => i.id !== id);
+  res.status(204).send();
+});
